33609f066ac5601a7bb45b0671dbc73b9468126a
abhishek <uxdesigns289@gmail.com>
2024-06-05 18:40:55 +0530

    working on API generator

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        InfyOm\GeneratorBuilder\GeneratorBuilderServiceProvider::class,
+        App\Providers\InfyOmGeneratorServiceProvider::class,
     ])->toArray(),
 
     /*
